9b7f1c637db4824a7d04297464eeb493bec7f4b3
daishin <daishindeveloper@gmail.com>
2023-08-19 19:29:22 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,101 @@
+let showFlag = document.querySelector(".showFlag");
+let flagImg = document.querySelector(".flagImg");
+let flagP = document.querySelector(".flagP");
+let guessInput = document.getElementById("guessInput");
+let submitButton = document.getElementById("submitButton");
+let startGameButton = document.getElementById("startGameButton");
+let pointsP = document.querySelector(".pointsP");
+let livesP = document.querySelector(".livesP");
+let countryList = document.getElementById("countryList");
+let bottomUI = document.querySelector(".bottomUI");
+let answerCommonName;
+let answerOffName;
+let guessAnswer;
+let points;
+let lives;
+
+setDefaultPoints = () => {
+  points = 0;
+  lives = 3;
+};
+
+async function startNewGame() {
+  let randomNumber = Math.floor(Math.random() * 251);
+  const response = await axios.get("https://restcountries.com/v3.1/all");
+  const data = response.data;
+  const currCountry = data[randomNumber];
+
+  //clear list
+  const countryNames = data.map((country) => country.name.common);
+  while (countryList.firstChild) {
+    countryList.removeChild(countryList.firstChild);
+  }
+
+  countryNames.forEach((name) => {
+    const option = document.createElement("option");
+    option.value = name;
+    countryList.appendChild(option);
+  });
+
+  guessInput.value = "";
+  guessInput.disabled = false;
+  submitButton.disabled = false;
+  answerCommonName = currCountry.name.common.toLowerCase();
+  answerOffName = currCountry.name.official.toLowerCase();
+  console.log(currCountry);
+  flagImg.src = currCountry.flags.png;
+
+  startGameButton.style.display = "none";
+  bottomUI.style.display = "flex";
+  setDefaultPoints();
+  livesP.innerText = "Lives: " + lives;
+  pointsP.innerText = "Points: " + points;
+}
+
+async function fetchNewData() {
+  try {
+    let randomNumber = Math.floor(Math.random() * 251);
+    const response = await axios.get("https://restcountries.com/v3.1/all");
+    const data = response.data;
+    const currCountry = data[randomNumber];
+    guessInput.value = "";
+    guessInput.disabled = false;
+    submitButton.disabled = false;
+    answerCommonName = currCountry.name.common.toLowerCase();
+    answerOffName = currCountry.name.official.toLowerCase();
+    console.log(currCountry);
+    flagImg.src = currCountry.flags.png;
+
+    startGameButton.style.display = "none";
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function handleSubmit() {
+  guessInputAnswer = guessInput.value.toLowerCase();
+  if (guessInputAnswer != "") {
+    if (
+      guessInputAnswer == answerCommonName ||
+      guessInputAnswer == answerOffName
+    ) {
+      console.log("You are right!");
+      guessInput.value = "";
+      fetchNewData();
+      points++;
+      pointsP.innerText = "Points: " + points;
+    } else {
+      console.log("Nothing happened");
+      console.log(guessInputAnswer);
+      console.log(answerCommonName);
+      lives--;
+      livesP.innerText = "Lives: " + lives;
+      if (lives == 0) {
+        alert("You lost!");
+        guessInput.disabled = true;
+        submitButton.disabled = true;
+        startGameButton.style.display = "block";
+      }
+    }
+  }
+}
